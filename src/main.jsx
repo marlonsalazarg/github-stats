738cd237e0e7837d5319bed3ca09530e3128dd9c
marlonsalazarg <marlon456_456@hotmail.com>
2023-10-31 18:14:06 -0500

    update routes on main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { AuthProvider } from "./context/Auth-Context.jsx";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Global } from "@emotion/react";
+import { reset, global } from "./styles/global.js";
+import SearchPage from "./pages/Search-Page.jsx";
+import ProfilePage from "./pages/Profile-Page.jsx";
+import SignUpForm from "./components/SignUp-Form.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/search",
+    element: <SearchPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpForm />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Global styles={reset} />
+    <Global styles={global} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );

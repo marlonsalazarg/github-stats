f11aa0bc63cb972daed1aba5d3953fca42ebc264
marlonsalazarg <marlon456_456@hotmail.com>
2023-10-31 17:40:37 -0500

    add api fecth

---
+++ src/services/api-fetch.js
@@ -0,0 +1,57 @@
+import { tokenKey, BASE_URI } from "../config";
+
+export default async function apiFetch(
+  endpoint,
+  { method, headers, body } = {}
+) {
+  const token = sessionStorage.getItem(tokenKey);
+
+  if (token) {
+    headers = {
+      Authorization: `Bearer ${token}`,
+      ...headers,
+    };
+  }
+
+  if (body) {
+    headers = {
+      "Content-Type": "application/json",
+      ...headers,
+    };
+  }
+
+  const config = {
+    method: method || (body ? "POST" : "GET"),
+    headers,
+    body: body ? JSON.stringify(body) : null,
+  };
+
+  const response = await fetch(BASE_URI + endpoint, config);
+
+  let data;
+  if (!response.ok) {
+    if(sessionStorage.getItem(tokenKey) && response.status === 401){
+      sessionStorage.removeItem(tokenKey);
+      window.location.reload();
+    }
+    try {
+      data = await response.json();
+    } catch (error) {
+      throw new Error(response.statusText);
+    }
+    if (typeof data.errors === "object"){
+      const errors = Object.entries(data.errors);
+      const messages = errors.map(([key, value])=> `${key} ${value}`);
+      data.errors = messages;
+    }
+    throw new Error(data.errors);
+  }
+
+  try {
+    data = await response.json();
+  } catch (error) {
+    data = response.statusText;
+  }
+
+  return data;
+}

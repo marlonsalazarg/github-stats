73c071111121b9f5860dc624591a0f0c6560c1f1
marlonsalazarg <marlon456_456@hotmail.com>
2023-10-31 17:53:40 -0500

    add signup form component

---
+++ src/components/SignUp-Form.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import Input from "./Input";
+import styled from "@emotion/styled";
+import { useAuth } from "../context/Auth-Context";
+import { Link } from "react-router-dom";
+import Container from "./Container";
+
+const CustomButton = styled.button`
+  background: #2d9cdb;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5em 1em;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  &:hover {
+    color: black;
+  }
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+  });
+  const { signup } = useAuth();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    signup(formData);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="example@mail.com"
+          label="Email"
+        />
+        <Input
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="******"
+          label="Password"
+        />
+        <Input
+          name="first_name"
+          value={formData.first_name}
+          onChange={handleChange}
+          label="First Name"
+        />
+        <Input
+          name="last_name"
+          value={formData.last_name}
+          onChange={handleChange}
+          label="Last Name"
+        />
+        <CustomButton type="submit">Create Account</CustomButton>
+        <Link to="/">Login</Link>
+      </form>
+    </Container>
+  );
+};
+
+export default SignUpForm;
